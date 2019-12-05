4dce2be70e4b279427ad9f5fc28645c0b03f2aed
Haas <haasj22@egr-linux-6456.campus.up.edu>
2019-12-05 10:02:31 -0800

    merged with new

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake, Miggy Sabater
+ * @author Tanya L. Crenshaw, Nathan Schmedake, John Haas
  * @since August 2013
  *
  */
